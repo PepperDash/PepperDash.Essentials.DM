cf3cc1c61edbf3f6d1cede037b7a13d1d7273e88
Andrew Knous <aknous@pepperdash.com>
2024-05-07 23:54:09 -0400

    feat: implements IMatrixRouting

---
+++ src/PepperDash.Essentials.DM/Routing/DmMatrixClearInput.cs
@@ -0,0 +1,25 @@
+﻿using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Routing;
+using System;
+
+namespace PepperDash.Essentials.DM.Routing
+{
+    public class DmMatrixClearInput : IRoutingInputSlot
+    {
+        public string TxDeviceKey => string.Empty;
+
+        public int SlotNumber => 0;
+
+        public eRoutingSignalType SupportedSignalTypes => eRoutingSignalType.AudioVideo;
+
+        public string Name => "None";
+
+        public BoolFeedback IsOnline => new BoolFeedback(() => false);
+
+        public bool VideoSyncDetected => false;
+
+        public string Key => "none";
+
+        public event EventHandler VideoSyncChanged;
+    }
+}
