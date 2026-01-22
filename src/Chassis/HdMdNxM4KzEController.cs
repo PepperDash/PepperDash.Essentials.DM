31e328799ef12b46b71b7ad9fc63743fe483da76
jkdevito <jdevito@pepperdash.com>
2026-01-22 12:29:39 -0600

    fix: update IsOnline feedback references in HdMdNxM4KzEController

--- src/Chassis/HdMdNxM4KzEController.cs
+++ src/Chassis/HdMdNxM4KzEController.cs
@@ -37,7 +37,6 @@ namespace PepperDash.Essentials.DM.Chassis
 		public FeedbackCollection<StringFeedback> OutputNameFeedbacks { get; private set; }
 		public FeedbackCollection<StringFeedback> OutputRouteNameFeedbacks { get; private set; }
 		public FeedbackCollection<BoolFeedback> InputHdcpEnableFeedback { get; private set; }
-		public BoolFeedback IsOnlineFeedback { get; private set;}
 		public StringFeedback DeviceNameFeedback { get; private set; }
 		public BoolFeedback AutoRouteFeedback { get; private set; }
 		public string NoRouteText { get; private set; }
@@ -82,7 +81,6 @@ namespace PepperDash.Essentials.DM.Chassis
 				OutputNames = props.Outputs;
 			}
 
-			IsOnlineFeedback = new BoolFeedback("IsOnline", () => _Chassis.IsOnline);
 			DeviceNameFeedback = new StringFeedback("DeviceName", () => Name);
 
 			VideoInputSyncFeedbacks = new FeedbackCollection<BoolFeedback>();
@@ -258,7 +256,7 @@ namespace PepperDash.Essentials.DM.Chassis
 
 		public void AddFeedbackCollections()
 		{
-			AddFeedbackToList(IsOnlineFeedback);
+			AddFeedbackToList(IsOnline);
 			AddFeedbackToList(DeviceNameFeedback);
 
 			if (AutoRouteFeedback != null)
@@ -374,7 +372,7 @@ namespace PepperDash.Essentials.DM.Chassis
 				Debug.LogInformation(this, "Please update config to use 'eiscapiadvanced' to get all join map features for this device.");
 			}
 
-			IsOnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
+			IsOnline.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
 			DeviceNameFeedback.LinkInputSig(trilist.StringInput[joinMap.Name.JoinNumber]);
 
 			if (_Chassis is HdMd4x14kzE _chassis)
@@ -442,7 +440,7 @@ namespace PepperDash.Essentials.DM.Chassis
 
 		private void UpdateFeedbacks()
 		{
-			IsOnlineFeedback?.FireUpdate();
+			IsOnline?.FireUpdate();
 			DeviceNameFeedback?.FireUpdate();
 			AutoRouteFeedback?.FireUpdate();
 
@@ -493,8 +491,8 @@ namespace PepperDash.Essentials.DM.Chassis
 			// TODO - Remove after testing
 			Debug.LogInformation(this, $"Chassis_OnlineStatusChange: DeviceOnline = {args.DeviceOnLine}");
 
-			IsOnlineFeedback.FireUpdate();
-
+			IsOnline.FireUpdate();
+			
 			if (!args.DeviceOnLine) return;
 
 			// TODO - Remove after testing
